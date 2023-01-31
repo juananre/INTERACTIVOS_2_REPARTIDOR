1c82c9c871ae219f1469468a5941e62bc8abc1d7
Carlos Andrés Escobar López <carlosa.escobar@upb.edu.co>
2023-01-31 14:12:05 -0500

    Cubito + Script

---
+++ interactivos 2/Assets/scripts/comm.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class comm : MonoBehaviour
+{
+
+    private static comm instance;
+    private Thread receiveThread;
+    private UdpClient receiveClient;
+    private IPEndPoint receiveEndPoint;
+    public string ip = "0";
+    public int receivePort = 32002;
+    private bool isInitialized;
+    private Queue receiveQueue;
+    public GameObject cube;
+    private Material m_Material;
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    private void Start()
+    {
+        m_Material = cube.GetComponent<Renderer>().material;
+    }
+
+    private void Initialize()
+    {
+        instance = this;
+        receiveEndPoint = new IPEndPoint(IPAddress.Parse(ip), receivePort); //recibe la informacion
+        receiveClient = new UdpClient(receivePort);
+        receiveQueue = Queue.Synchronized(new Queue()); //puerta
+        receiveThread = new Thread(new ThreadStart(ReceiveDataListener)); //Crear nuevo hilo
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+        isInitialized = true;
+    }
+
+    private void ReceiveDataListener()
+    {
+        while (true)
+        {
+            try
+            {
+                byte[] data = receiveClient.Receive(ref receiveEndPoint);
+                string text = Encoding.UTF8.GetString(data);
+                SerializeMessage(text);
+            }
+            catch (System.Exception ex)
+            {
+                Debug.Log(ex.ToString());
+            }
+        }
+    }
+
+    private void SerializeMessage(string message)
+    {
+        try
+        {
+            string[] chain = message.Split(' ');
+            string key = chain[0];
+            float value = 0;
+            if (float.TryParse(chain[1], out value))
+            {
+                receiveQueue.Enqueue(value);
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    private void OnDestroy()
+    {
+        TryKillThread();
+    }
+
+    private void OnApplicationQuit()
+    {
+        TryKillThread();
+    }
+
+    private void TryKillThread()
+    {
+        if (isInitialized)
+        {
+            receiveThread.Abort();
+            receiveThread = null;
+            receiveClient.Close();
+            receiveClient = null;
+            Debug.Log("Thread killed");
+            isInitialized = false;
+        }
+    }
+
+    void Update()
+    {
+        if (receiveQueue.Count != 0)
+        {
+            float counter = (float)receiveQueue.Dequeue();
+
+            if (counter == 1F) m_Material.color = Color.black;
+            if (counter == 2F) m_Material.color = Color.red;
+        }
+
+    }
+
+}

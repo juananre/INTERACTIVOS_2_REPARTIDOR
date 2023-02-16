e951089b8fb4eca738ffcc9e367109ed1dae8f5f
Carlos Andrés Escobar López <carlosa.escobar@upb.edu.co>
2023-02-16 11:32:03 -0500

    Caja Cambios

--- interactivos 2/Assets/PlayerController.cs
+++ interactivos 2/Assets/PlayerController.cs
@@ -1,19 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-   public float speed = 100f;
+    public byte run;
+    public float cambio = 0f;
+    public float speed = 0f;
     private Rigidbody rb;
+   
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
-        // mueve el objeto hacia la derecha
-        transform.Translate(Vector3.fwd * Time.deltaTime * speed);
+         
+         transform.Translate(Vector3.fwd * Time.deltaTime * speed);
 
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            cambio += 10f;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            cambio -= 10f;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) { run =1;}
+        if (Input.GetKeyDown(KeyCode.V)) { run = 0; }
+        if ((speed < cambio)) { MasSpeed(); } else { MenosSpeed(); }
+        if (speed <= 0) speed = 0;
+        if (cambio <= 0) cambio = 0;
+        if (cambio >= 50) cambio = 50;
+        
     }
+    void MasSpeed()
+    {   if (run == 1) { speed += 0.05f;}
+     
+    }
+    void MenosSpeed()
+    {
+         speed -= 0.05f; 
+    }
+
 }

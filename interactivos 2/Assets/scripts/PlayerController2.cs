3f2fdc00d3d3023a7c2e7f774ad2ff48835d3795
Carlos Andrés Escobar López <carlosa.escobar@upb.edu.co>
2023-03-06 17:21:33 -0500

    Acelerador funcionando

--- interactivos 2/Assets/scripts/PlayerController2.cs
+++ interactivos 2/Assets/scripts/PlayerController2.cs
@@ -4,6 +4,8 @@ using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using System.IO.Ports;
+using System;
 
 public class PlayerController2 : MonoBehaviour
 {
@@ -11,12 +13,37 @@ public class PlayerController2 : MonoBehaviour
     public float cambio = 0f;
     public float speed = 0f;
     private Rigidbody rb;
+    private bool acelerador = false;
+    private SerialPort _serialPort;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+        openSerialPort();
+
+
     }
+
+    private void openSerialPort()
+    {
+        try
+        {
+            _serialPort = new SerialPort();
+            _serialPort.PortName = "COM3";
+            _serialPort.BaudRate = 115200;
+            _serialPort.DtrEnable = true;
+            _serialPort.NewLine = "\n";
+            _serialPort.Open();
+            Debug.Log("Open Serial Port");
+
+        }
+        catch(Exception e) { 
+            Debug.Log(e);
+
+       }
+
+    }
+
     void Update()
     {
 
@@ -25,6 +52,22 @@ public class PlayerController2 : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.C)) { run = 1; }
         if (Input.GetKeyDown(KeyCode.V)) { run = 0; }
+
+        if (_serialPort.BytesToRead > 0)
+        {
+            string response = _serialPort.ReadLine();
+            if (response == "accPressed")
+            {
+                acelerador = true;
+                Debug.Log("accPressed");
+            }
+            else if (response == "accReleased")
+            {
+                Debug.Log("accReleased");
+                acelerador = false;
+            }
+        }
+
         if ((speed < cambio)) { MasSpeed(); } else { MenosSpeed(); }
         if (run == 0) MenosSpeed();
         if (speed <= 0) speed = 0;
@@ -36,9 +79,10 @@ public class PlayerController2 : MonoBehaviour
     }
     void MasSpeed()
     {
-        if (run == 1) { if ((Input.GetKey(KeyCode.Space))) { speed += 0.02f; } else { MenosSpeed(); } }
-
+        // if (run == 1) { if ((Input.GetKey(KeyCode.Space))) { speed += 0.02f; } else { MenosSpeed(); } }
+        if (run == 1) { if (true == acelerador) { speed += 0.02f; } else { MenosSpeed(); } }
     }
+
     void MenosSpeed()
     {
         speed -= 0.02f;

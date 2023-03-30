af31d4e84422591f2e1582d3d83803fc9da9ab91
juananre <78058130+juananre@users.noreply.github.com>
2023-03-30 02:10:47 -0500

    audio

--- interactivos 2/Assets/scripts/PlayerController2.cs
+++ interactivos 2/Assets/scripts/PlayerController2.cs
@@ -12,6 +12,11 @@ public class PlayerController2 : MonoBehaviour
     public float speed = 0f;
     private Rigidbody rb;
 
+    [Header("Audios")]
+    [SerializeField] AudioSource audio_Arranque;
+    [SerializeField] AudioSource audio_neutro;
+    [SerializeField] AudioSource audio_movimiento;
+    [SerializeField] AudioSource audio_freno;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -22,10 +27,24 @@ public class PlayerController2 : MonoBehaviour
 
         transform.Translate(Vector3.down * Time.deltaTime * speed);
 
-        
-        if (Input.GetKeyDown(KeyCode.C)) { run = 1; }
-        if (Input.GetKeyDown(KeyCode.V)) { run = 0; }
-        if ((speed < cambio)) { MasSpeed(); } else { MenosSpeed(); }
+
+        if (Input.GetKeyDown(KeyCode.C)) 
+        {
+            run = 1;
+            encendido();
+        }
+        if (Input.GetKeyDown(KeyCode.V)) 
+        {
+            run = 0; 
+        }
+        if ((speed < cambio)) 
+        { 
+            MasSpeed(); 
+        } 
+        else 
+        {
+            MenosSpeed(); 
+        }
         if (run == 0) MenosSpeed();
         if (speed <= 0) speed = 0;
         if (cambio <= 0) cambio = 0;
@@ -36,8 +55,21 @@ public class PlayerController2 : MonoBehaviour
     }
     void MasSpeed()
     {
-        if (run == 1) { if ((Input.GetKey(KeyCode.Space))) { speed += 0.02f; } else { MenosSpeed(); } }
-
+        if (run == 1)
+        { 
+            if (Input.GetKey(KeyCode.Space)) 
+            { 
+                speed += 0.02f;
+                audio_neutro.Stop();
+                audio_movimiento.Play();
+            }
+            else 
+            {
+                MenosSpeed();
+                audio_movimiento.Stop();
+                audio_freno.Play();
+            }
+        }
     }
     void MenosSpeed()
     {
@@ -45,12 +77,16 @@ public class PlayerController2 : MonoBehaviour
     }
     public void sube()
     {
-        Debug.Log("si");
         cambio += 2f;
     }
     public void baja()
     {
-        Debug.Log("si");
         cambio -= 2f;
     }
+    public void encendido()
+    {
+        audio_Arranque.Play();
+        audio_neutro.PlayDelayed(2f);
+    }
+ 
 }

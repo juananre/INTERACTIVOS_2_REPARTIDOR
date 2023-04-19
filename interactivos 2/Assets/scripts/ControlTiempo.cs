ff32bc14b66ef86bbb660f7858cf362fb7e1df18
Carlos A <kasacoop23@gmail.com>
2023-04-19 16:43:56 -0500

    TIempo

---
+++ interactivos 2/Assets/scripts/ControlTiempo.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using Unity.VisualScripting;
+
+public class ControlTiempo : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text txt_contador_Tiempo;
+    private int contador_Tiempo = 60;
+    private int i = 0;
+    public static ControlTiempo Instance { get; private set; }
+
+
+    void Start()
+    {
+        Invoke("TerminarTemporizador", 600f);
+
+        InvokeRepeating("ActualizarTiempo", 0f, 1f);
+    }
+
+    public void ActualizarTiempo()
+    {
+        contador_Tiempo -= 1;
+        ActualizarValorUI();
+    }
+    public void SumarTiempo(int valor)
+    {
+        contador_Tiempo += valor;
+        ActualizarValorUI();
+    }
+    public void ActualizarValorUI()
+    {
+        txt_contador_Tiempo.text = "" + contador_Tiempo;
+    }
+    void TerminarTemporizador()
+    {
+        Debug.Log("Se termino el tiempo");
+
+    }
+    public void Awake()
+    {
+        if (Instance != null && Instance != this) { Destroy(this); } else { Instance = this; }
+    }
+}

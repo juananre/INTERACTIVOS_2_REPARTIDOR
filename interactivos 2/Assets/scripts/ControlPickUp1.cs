bead5702b9828f4cbe09b9b84ce5e1e814d250c3
juananre <78058130+juananre@users.noreply.github.com>
2023-05-23 21:46:33 -0500

    Revert "Revert "Merge branch 'delevoper'""
    This reverts commit b4513c08e2be28c279844849a8d648f4b4f50752.

---
+++ interactivos 2/Assets/scripts/ControlPickUp1.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
+
+
+public class ControlPickUp1 : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text txt_contador_pickup1;
+    public static int contador_pickup1 = 6;
+
+    
+
+    public void Start()
+    {
+        
+    }
+    public int CantidadPickUpsRecolectados()
+    {
+
+        return contador_pickup1;
+    }
+    public void ActualizarPuntaje(int valor)
+    {
+        
+            contador_pickup1 -= valor;
+            ActualizarValorUI();
+        
+           
+    }
+    public void RestablecerPuntaje()
+    {
+
+        if (contador_pickup1 < 6)
+        {
+            Debug.Log("AYAYAY");
+            contador_pickup1 += 1;
+            ActualizarValorUI();
+        }
+       
+    }
+    private void ActualizarValorUI()
+    {
+        if (contador_pickup1 == 0)
+        {
+            SceneManager.LoadScene(3);
+        }
+        txt_contador_pickup1.text = "" + contador_pickup1;
+    }
+}
